1ff97ea00c63e402ecbe4100604f8ee7a783871f
Alexey Tyulenev <lexatul@gmail.com>
2023-11-13 13:28:18 +0300

    Change poincuts

--- src/main/java/ru/tyulenev/cars_garage_distributor/aspects/CustomPointcuts.java
+++ src/main/java/ru/tyulenev/cars_garage_distributor/aspects/CustomPointcuts.java
@@ -3,6 +3,6 @@ package ru.tyulenev.cars_garage_distributor.aspects;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class CustomPointcuts {
-    @Pointcut("execution(public void addNewCar(*))")
+    @Pointcut("execution(public * addNewCar(*))")
     public void addCarMethod() {}
 }
